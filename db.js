59afa2d0644e087130e1bdfecd77e27e1824077f
Michael Z <flashman123@fastmail.fm>
2021-09-02 17:14:22 +0100

    name changed to email models/user.js, routes/user.js, utils/user.js

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect(`mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`, 
-    {useNewUrlParser: true, useUnifiedTopology: true},
-    console.log("Connecton found.")
-);
-
-// const connection = mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.dbst6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, 
+// const connection = mongoose.connect(`mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`, 
 //     {useNewUrlParser: true, useUnifiedTopology: true},
 //     console.log("Connecton found.")
 // );
+
+const connection = mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.dbst6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, 
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    console.log("Connecton found.")
+);
 
 module.exports = connection;
 
--- utils/user.js
+++ utils/user.js
@@ -1,11 +1,11 @@
 
 const User = require("../models/user");
 
-const addUser = async (name, passwordHash) => {
+const addUser = async (email, passwordHash) => {
     
     try {
 
-    const newUser = new User({name, passwordHash});
+    const newUser = new User({email, passwordHash});
     await newUser.save();
     console.log("addUser has been reached")
     console.log(newUser);
