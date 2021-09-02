5d6d2759a7952cb3ad9490a380b699f0ad0ff670
Michael Z <flashman123@fastmail.fm>
2021-09-02 11:47:17 +0100

    error.js and index.js error route added

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const express = require("express");
 const { connection } = require("./db.js");
 const port = process.env.PORT;
 const landingRouter = require("./routes/landingPage");
+const errorRouter = require("./routes/error");
 
 const app = express();
 
 app.use("/landingpage", landingRouter);
+app.use("*", errorRouter);
 
 app.listen(port, () => {
     console.log("listening");
--- routes/error.js
+++ routes/error.js
@@ -3,4 +3,6 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/", (req, res) => res.status(404).json({msg: "not found"}));
+
 module.exports = router;
