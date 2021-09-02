74f31a60099f2231a4cadd371780c945e8f2a776
Michael Z <flashman123@fastmail.fm>
2021-09-02 15:41:16 +0100

    register route complete and working with local db

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 
- const router = express.Router();
+const { addUser } = require("../utils/user");
 
- const saltRounds = 10;
+const router = express.Router();
+
+const saltRounds = 10;
+
 
 router.post("/", (req, res) => {
     res.status(200).send("landing to home root working");
@@ -12,17 +15,18 @@ router.post("/", (req, res) => {
 // reigster user
 
 router.post("/register", async (req, res) => {
-    // console.log(req.params)
-    // res.status(200).json({msg: `${req.body.name} ${req.body.password} ${req.body.checkPassword}`});
-    // console.log(req.body.name)
     if (req.body.password !== req.body.checkPassword) {
         return res.status(401).json({msg: "Your passwords do not match"});
     } else if (!req.body.name) {
         return res.status(401).json({msg: "Please enter your username"});
-    }
-
+    }    
+    
     const salt = await bcrypt.genSalt(saltRounds);
     const hash = await bcrypt.hash(req.body.password, salt);
+
+    await addUser(req.body.name, hash);
+    res.status(201).json({"message": "Created user"});
+    
 });
 
 router.post("/login", (req, res) => {
@@ -31,8 +35,3 @@ router.post("/login", (req, res) => {
 
  module.exports = router;
 
-//  if (req.body.password !== req.body.checkPassword) {
-//     return res.status(401).json({msg: "Your passwords do not match."});
-// } else if (!req.body.name) {
-//     return res.status(401).json({msg: "Please provide your username"});
-// }
