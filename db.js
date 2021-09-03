571f0e58ebf4f4538127d03463cc44d143ae0434
kjkent <kris@kriskent.com>
2021-09-03 02:01:17 +0100

    Installed nodemon dev dependency

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
-// const connection = mongoose.connect(`mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`, 
-//     {useNewUrlParser: true, useUnifiedTopology: true},
-//     console.log("Connecton found.")
-// );
-
 const connection = mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.dbst6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, 
     {useNewUrlParser: true, useUnifiedTopology: true},
     console.log("Connecton found.")
 );
 
 module.exports = connection;
+
+
+// const connection = mongoose.connect(`mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`, 
+//     {useNewUrlParser: true, useUnifiedTopology: true},
+//     console.log("Connecton found.")
+// );
 
 // mongodb+srv://<username>:<password>@cluster0.dbst6.mongodb.net/<database>?retryWrites=true&w=majority
