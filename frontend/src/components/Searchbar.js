81c77554d2ae2c589c0f868f211707ad003c65e7
Michael Z <flashman123@fastmail.fm>
2021-09-05 11:55:49 +0100

    search by name added Searchbar.js

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -3,25 +3,35 @@ import "./Searchbar.css";
 
 function Searchbar() { 
     return (
-        <div className ="search">
-            <div className="searchInputs">
-            <input type="text"></input>
-            <div className="searchIcon"></div>
-            </div> 
-            <div className ="dataResult"></div>
+        // <div className ="search">
+        //     <div className="searchInputs">
+        //         <label>Search by Name</label>
+        //         <input type="text" placeholder="Search by Name"></input>
+        //     <div className="searchIcon"></div>
+        // </div> 
+        // <div className ="dataResult"></div>
         
-        <div className="searchInputs">
-            <input type="text"></input>
-            <div className="searchIcon"></div>
-            </div> 
-            <div className ="dataResult"></div>
+        // <div className="searchInputs">
+        //     <input type="text"></input>
+        //     <div className="searchIcon"></div>
+        //     </div> 
+        //     <div className ="dataResult"></div>
 
-        <div className="searchInputs">
-            <input type="text"></input>
-            <div className="searchIcon"></div>
-            </div> 
-            <div className ="dataResult"></div>
-        </div> 
+        // <div className="searchInputs">
+        //     <input type="text"></input>
+        //     <div className="searchIcon"></div>
+        //     </div> 
+        //     <div className ="dataResult"></div>
+        // </div> 
+        <>
+            <div>
+                <form type="submit">
+                    <label>Search by Name</label>
+                    <input placeholder="type a movie name"></input>
+                    <button>Search</button>
+                </form>
+            </div>
+        </>
        
 );
 }
